9247d346778240305ea30078eee51261a94c66df
abhiram <abhiram@goscale.co>
2018-07-09 19:58:37 +0530

    java and jenkins

---
+++ java.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+apt-get install -y software-properties-common debconf-utils
+add-apt-repository -y ppa:webupd8team/java
+apt-get update
+echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+apt-get install -y oracle-java8-installer
+java --version
---
+++ jenkins-master.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+apt-get update
+apt-get install -y jenkins
+systemctl status jenkins
