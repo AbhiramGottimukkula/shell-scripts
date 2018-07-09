c49bb813b14ceec686a4b3265e3fc92614b49cdb
abhiram <abhiram@goscale.co>
2018-07-09 20:16:44 +0530

    typo

--- java.sh
+++ java.sh
@@ -4,4 +4,4 @@ add-apt-repository -y ppa:webupd8team/java
 apt-get update
 echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
 apt-get install -y oracle-java8-installer
-java --version
+java -version
