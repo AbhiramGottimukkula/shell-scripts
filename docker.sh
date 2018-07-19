d889e05a7fbc38f3b7dacd1cb5be1103b33c3644
AbhiramGottimukkula <38419855+AbhiramGottimukkula@users.noreply.github.com>
2018-07-19 15:21:15 +0530

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+apt-get update
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-cache policy docker-ce
+apt-get install -y docker-ce
+systemctl status docker
